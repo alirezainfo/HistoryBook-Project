5114564f968e60443a41caaf31639cc8ef16f362
Alireza Sadeghi <khodemte@gmail.com>
2026-08-17 17:35:50 +0330

    resolve vite config conflict

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-<<<<<<< HEAD
 export default defineConfig({
   base: '/HistoryBook-Project/',
 
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
 
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
->>>>>>> f091b63592f1025a7ffa36dbac656c512d6e9d88
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> f091b63592f1025a7ffa36dbac656c512d6e9d88
